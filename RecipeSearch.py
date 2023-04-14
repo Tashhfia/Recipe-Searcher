0fe255a9c0bccee238a1fdcaaf19271fece40e98
tashfia <hridishamim128@gmail.com>
2023-04-14 16:12:45 +0900

    previous project scratched

--- RecipeSearch.py
+++ RecipeSearch.py
@@ -2,6 +2,8 @@ from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+import time
 
 recipe = "hummus"
 
@@ -9,17 +11,41 @@ service = Service(executable_path="E:\Misc\SeleniumDrivers\chromedriver_win32\ch
 driver = webdriver.Chrome(service=service)
 
 
-driver.get("https://www.google.com/")
+driver.get("https://www.recipetineats.com/recipes/")
 
-# disable if default browser is in english
-driver.find_element(By.PARTIAL_LINK_TEXT, "English").click()
-search = driver.find_element(By.ID, "APjFqb")
+links = driver.find_elements(By.XPATH, "//h2/a[@rel]")
+for link in links:
+    print (link.get_attribute("href"))
 
-search.send_keys(f"Best {recipe} recipe", Keys.ENTER)
+# driver.find_element(By.CLASS_NAME, "general-search__icon-button").click()
+# search = driver.find_element(By.CLASS_NAME, 'general-search__input')
 
-elem = driver.find_elements(By.CSS_SELECTOR, "div[class^='YwonT'" )
-for e in elem:
-    print(e.text)
+# search.click()
+# time.sleep(5)
+# #driver.find_element(By.ID, 'fullscreen-nav__search__search-input').send_keys(f"Best {recipe} recipe", Keys.ENTER)
+# driver.find_element(By.XPATH, "/html/body/header/div[2]/nav/div[1]/form/div/input").send_keys(f"Best {recipe} recipe", Keys.ENTER)
 
-# but = driver.find_element(By.CLASS_NAME, "dU5Kl")
+#search.send_keys(f"Best {recipe} recipe", Keys.ENTER)
+
+# driver.find_element(By.CSS_SELECTOR, "div[aria-label ^= 'Show more']").click()
+
+# try:
+#     # elem = driver.find_elements(By.CSS_SELECTOR, "div[class^='YwonT'" )
+#     elem = driver.find_elements(By.CSS_SELECTOR, 'div[id = "isl_1-1"]')
+
+#     for k in elem:
+#         print(k.text)
+#     print("-----------------------")
+#     # for c in t:
+#     #     print(c.text)
+
+
+# except Exception as e:
+#     print("Element not found...!")
+#     print(e) 
+
+
+
+
+# but = 
 # but.click()
