174d75ab24ba84c42ffd931618caffb51def4154
tashfia <hridishamim128@gmail.com>
2023-04-15 01:44:06 +0900

    added try and except

--- RecipeSearch.py
+++ RecipeSearch.py
@@ -22,22 +22,25 @@ def findLinks(site):
     pageExists = True
     
     while pageExists:
-        # to avoid StaleElementReferenceException
-        time.sleep(5)
-        # finds all the links in the page
-        links = driver.find_elements(By.XPATH, "//h2/a[@rel]")
-        for link in links:
-            recipeLinks.append(link.get_attribute("href"))
+        try:
+            # to avoid StaleElementReferenceException
+            time.sleep(5)
+            # finds all the links in the page
+            links = driver.find_elements(By.XPATH, "//h2/a[@rel]")
+            for link in links:
+                recipeLinks.append(link.get_attribute("href"))
 
-        # finds the last button from a list of buttons
-        nextPage = driver.find_element(By.XPATH, "/html/body/div[1]/div[3]/div[3]/main/div/div/ul/li[last()]")
-    
-        if nextPage.get_attribute("class") != "pagination-next":
-            print("all pages scraped...")
-            pageExists = False
-        else:
-            # if its not the last page, then go to next page
-            nextPage.click()
+            # finds the last button from a list of buttons
+            nextPage = driver.find_element(By.XPATH, "/html/body/div[1]/div[3]/div[3]/main/div/div/ul/li[last()]")
+        
+            if nextPage.get_attribute("class") != "pagination-next":
+                print("all pages scraped...")
+                pageExists = False
+            else:
+                # if its not the last page, then go to next page
+                nextPage.click()
+        except:
+            print("Something went wrong!")
 
     print(len(recipeLinks))
     return recipeLinks
