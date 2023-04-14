32ccb887f54d3050d2dd26d48882d4f85c1b3da4
tashfia <hridishamim128@gmail.com>
2023-04-14 23:03:35 +0900

    links updated

--- RecipeSearch.py
+++ RecipeSearch.py
@@ -8,15 +8,16 @@ from selenium.webdriver.support.wait import WebDriverWait
 from bs4 import BeautifulSoup
 
 recipe = "hummus"
+site = "https://www.recipetineats.com/recipes/"
 
 service = Service(executable_path="E:\Misc\SeleniumDrivers\chromedriver_win32\chromedriver.exe")
 driver = webdriver.Chrome(service=service)
 
-def find_links():
+def findLinks(site):
 
     """Finds links of all the recipes in the website"""
 
-    driver.get("https://www.recipetineats.com/recipes/")
+    driver.get(site)
     recipeLinks = []
     pageExists = True
     
@@ -41,35 +42,9 @@ def find_links():
     print(len(recipeLinks))
     return recipeLinks
 
-# driver.find_element(By.CLASS_NAME, "general-search__icon-button").click()
-# search = driver.find_element(By.CLASS_NAME, 'general-search__input')
+# uncommnent only if you lose the links
+# link_list = findLinks(site)
+# with open('recipe_links.txt', 'w') as f:
+#     for r in link_list:
+#         f.write(f"{r}\n")
 
-# search.click()
-# time.sleep(5)
-# #driver.find_element(By.ID, 'fullscreen-nav__search__search-input').send_keys(f"Best {recipe} recipe", Keys.ENTER)
-# driver.find_element(By.XPATH, "/html/body/header/div[2]/nav/div[1]/form/div/input").send_keys(f"Best {recipe} recipe", Keys.ENTER)
-
-#search.send_keys(f"Best {recipe} recipe", Keys.ENTER)
-
-# driver.find_element(By.CSS_SELECTOR, "div[aria-label ^= 'Show more']").click()
-
-# try:
-#     # elem = driver.find_elements(By.CSS_SELECTOR, "div[class^='YwonT'" )
-#     elem = driver.find_elements(By.CSS_SELECTOR, 'div[id = "isl_1-1"]')
-
-#     for k in elem:
-#         print(k.text)
-#     print("-----------------------")
-#     # for c in t:
-#     #     print(c.text)
-
-
-# except Exception as e:
-#     print("Element not found...!")
-#     print(e) 
-
-
-
-
-# but = 
-# but.click()
