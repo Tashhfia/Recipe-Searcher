4e53c696c561358f1dc6eee5bd5e1edbbb3e3353
tashfia <hridishamim128@gmail.com>
2023-04-14 22:34:31 +0900

    added comments to find links function

--- RecipeSearch.py
+++ RecipeSearch.py
@@ -12,25 +12,30 @@ recipe = "hummus"
 service = Service(executable_path="E:\Misc\SeleniumDrivers\chromedriver_win32\chromedriver.exe")
 driver = webdriver.Chrome(service=service)
 
-
-driver.get("https://www.recipetineats.com/recipes/")
-
 def find_links():
+
+    """Finds links of all the recipes in the website"""
+
+    driver.get("https://www.recipetineats.com/recipes/")
     recipeLinks = []
     pageExists = True
+    
     while pageExists:
         # to avoid StaleElementReferenceException
         time.sleep(5)
+        # finds all the links in the page
         links = driver.find_elements(By.XPATH, "//h2/a[@rel]")
         for link in links:
             recipeLinks.append(link.get_attribute("href"))
 
+        # finds the last button from a list of buttons
         nextPage = driver.find_element(By.XPATH, "/html/body/div[1]/div[3]/div[3]/main/div/div/ul/li[last()]")
     
         if nextPage.get_attribute("class") != "pagination-next":
             print("all pages scraped...")
             pageExists = False
         else:
+            # if its not the last page, then go to next page
             nextPage.click()
 
     print(len(recipeLinks))
